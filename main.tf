a6c108ef827543bcef865603b5d8ccbd7e8e2f07
SherifEssamGomaa <sherifessamahmed@gmail.com>
2023-01-23 17:25:01 +0200

    modifictions

--- main.tf
+++ main.tf
@@ -9,27 +9,40 @@ resource "google_compute_subnetwork" "managment-subnetwork" {
   name          = "managment-subnetwork"
   ip_cidr_range = "10.0.1.0/24"
   network       = google_compute_network.vpc-network.id 
+  depends_on = [
+    google_compute_network.vpc-network
+  ]
 }
 
 resource "google_compute_subnetwork" "restricted-subnetwork" {
   name          = "restricted-subnetwork"
   ip_cidr_range = "10.0.2.0/24"
   network       = google_compute_network.vpc-network.id 
+  depends_on = [
+    google_compute_network.vpc-network
+  ]
 }
 
 resource "google_compute_instance" "private-instance" {
   allow_stopping_for_update = true
   name         = "private-instance"
   machine_type = "e2-micro"
+  zone         = "us-central1-a"
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      type = "pd-standard"
+      size = 10 
     }
   }
   network_interface {
     subnetwork = "managment-subnetwork"
   }
   tags = ["allow-ssh"]
+  depends_on = [
+    google_compute_subnetwork.managment-subnetwork,
+    google_compute_firewall.allow-ssh-rule
+  ]
 }
 
 resource "google_compute_firewall" "allow-ssh-rule" {
