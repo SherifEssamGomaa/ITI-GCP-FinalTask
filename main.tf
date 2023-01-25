6eef7c3976b32ff7f6f77b197d660ee0308eb371
SherifEssamGomaa <sherifessamahmed@gmail.com>
2023-01-25 16:42:53 +0200

    added a startup script for the instance

--- main.tf
+++ main.tf
@@ -71,25 +71,7 @@ resource "google_compute_instance" "private-instance" {
     google_compute_subnetwork.managment-subnetwork,
     google_compute_firewall.allow-ssh-rule
   ]
-  metadata_startup_script = <<-EOF
-    sudo apt-get install  -y apt-transport-https ca-certificates gnupg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    sudo apt-get update && sudo apt-get install google-cloud -y
-
-
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    chmod +x kubectl
-    mkdir -p ~/.local/bin
-    mv ./kubectl ~/.local/bin/kubectl
-    kubectl version --client
-    sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-    gcloud container clusters get-credentials app-cluster --zone europe-west1-c --project eminent-subset-375011
-  EOF
-  
+  metadata_startup_script = "${file("./script.sh")}"
 }
 
 resource "google_compute_firewall" "allow-ssh-rule" {
